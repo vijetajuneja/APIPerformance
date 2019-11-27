e6e6c544e37743d73c60c79373afc3837f9184c4
Vijeta <Vijeta@LAPTOP-8TV3KQUH>
2019-11-28 01:04:33 +0530

    commented GC classes

--- src/test/java/TestScenario/ValidateAfterPay.java
+++ src/test/java/TestScenario/ValidateAfterPay.java
@@ -1,64 +1,64 @@
-package TestScenario;
-
-import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
-import com.tommyjohn.automation.Pages.AccountLoginPage;
-import com.tommyjohn.automation.Pages.AfterPay;
-import com.tommyjohn.automation.utils.CustomUtilities;
-
-public class ValidateAfterPay {
-
-	WebDriver driver ;
-
-	@BeforeMethod(alwaysRun = true)
-	public void setup() {
-		try {
-			driver = new CustomUtilities().launchtj();
-
-		} catch (Exception e1) {
-			e1.printStackTrace();
-		}
-	}
-	
-	@Test
-	public void TC_ValidateAfterPayOnPDP() throws Exception
-	{
-		new AfterPay(driver).AfterPayOnPDP();
-	}
-	
-	@Test
-	public void TC_ValidateAfterPayOnInlineCart() throws Exception
-	{
-		new AfterPay(driver).AfterPayOnInlineCart();
-	}
-	
-	@Test
-	public void TC_ValidateAfterPayOnShoppingCart() throws Exception
-	{
-		new AfterPay(driver).AfterPayOnShoppingCart();
-	}
-	
-	@Test
-	public void TC_ValidateAfterPayOnCheckoutPage() throws Exception
-	{
-		new AfterPay(driver).AfterPayOnCheckoutPage();
-	}
-	
-//	@Test
-//	public void TC_ValidateAfterPayWithGiftCard() throws Exception
-//	{
-//		new AfterPay(driver).AfterPayOnGiftCardPage();
+//package TestScenario;
+//
+//import org.openqa.selenium.WebDriver;
+//import org.testng.annotations.AfterMethod;
+//import org.testng.annotations.BeforeMethod;
+//import org.testng.annotations.Test;
+//
+//import com.tommyjohn.automation.Pages.AccountLoginPage;
+//import com.tommyjohn.automation.Pages.AfterPay;
+//import com.tommyjohn.automation.utils.CustomUtilities;
+//
+//public class ValidateAfterPay {
+//
+//	WebDriver driver ;
+//
+//	@BeforeMethod(alwaysRun = true)
+//	public void setup() {
+//		try {
+//			driver = new CustomUtilities().launchtj();
+//
+//		} catch (Exception e1) {
+//			e1.printStackTrace();
+//		}
 //	}
-
-
-	@AfterMethod(alwaysRun = true)
-	void teardown () {
-		
-		driver.close();
-		driver.quit();
-	}
-
-}
+//	
+//	@Test
+//	public void TC_ValidateAfterPayOnPDP() throws Exception
+//	{
+//		new AfterPay(driver).AfterPayOnPDP();
+//	}
+//	
+//	@Test
+//	public void TC_ValidateAfterPayOnInlineCart() throws Exception
+//	{
+//		new AfterPay(driver).AfterPayOnInlineCart();
+//	}
+//	
+//	@Test
+//	public void TC_ValidateAfterPayOnShoppingCart() throws Exception
+//	{
+//		new AfterPay(driver).AfterPayOnShoppingCart();
+//	}
+//	
+//	@Test
+//	public void TC_ValidateAfterPayOnCheckoutPage() throws Exception
+//	{
+//		new AfterPay(driver).AfterPayOnCheckoutPage();
+//	}
+//	
+////	@Test
+////	public void TC_ValidateAfterPayWithGiftCard() throws Exception
+////	{
+////		new AfterPay(driver).AfterPayOnGiftCardPage();
+////	}
+//
+//
+//	@AfterMethod(alwaysRun = true)
+//	void teardown () {
+//		
+//		driver.close();
+//		driver.quit();
+//	}
+//
+//}
--- src/test/java/TestScenario/ValidateGiftCardFunctionality.java
+++ src/test/java/TestScenario/ValidateGiftCardFunctionality.java
@@ -1,50 +1,50 @@
-package TestScenario;
-
-import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
-import com.tommyjohn.automation.Pages.GiftCardPage;
-import com.tommyjohn.automation.utils.CustomUtilities;
-
-public class ValidateGiftCardFunctionality{
-
-	WebDriver driver;
-
-	@BeforeMethod(alwaysRun = true)
-	public void setup() {
-		try {
-			driver = new CustomUtilities().launchtj();
-
-		} catch (Exception e1) {
-			e1.printStackTrace();
-		}
-	}
-
-	@Test
-	public void ValidateGiftCardFlow() throws Exception
-	{
-		new GiftCardPage(driver).giftCardPageCheck();
-	}
-
-	@Test
-	public void FieldValidationsForGiftCard() throws Exception
-	{
-		new GiftCardPage(driver).verifyfieldsforGCPage();
-	}
-
-	@Test
-	public void ValidateErrorScenariosForGiftCard() throws Exception
-	{
-		new GiftCardPage(driver).verifyerrormessagesongiftcard();
-	}
-
-	@AfterMethod(alwaysRun = true)
-	void teardown () {
-
-		driver.close();
-		driver.quit();
-	}
-
-}
+//package TestScenario;
+//
+//import org.openqa.selenium.WebDriver;
+//import org.testng.annotations.AfterMethod;
+//import org.testng.annotations.BeforeMethod;
+//import org.testng.annotations.Test;
+//
+//import com.tommyjohn.automation.Pages.GiftCardPage;
+//import com.tommyjohn.automation.utils.CustomUtilities;
+//
+//public class ValidateGiftCardFunctionality{
+//
+//	WebDriver driver;
+//
+//	@BeforeMethod(alwaysRun = true)
+//	public void setup() {
+//		try {
+//			driver = new CustomUtilities().launchtj();
+//
+//		} catch (Exception e1) {
+//			e1.printStackTrace();
+//		}
+//	}
+//
+//	@Test
+//	public void ValidateGiftCardFlow() throws Exception
+//	{
+//		new GiftCardPage(driver).giftCardPageCheck();
+//	}
+//
+//	@Test
+//	public void FieldValidationsForGiftCard() throws Exception
+//	{
+//		new GiftCardPage(driver).verifyfieldsforGCPage();
+//	}
+//
+//	@Test
+//	public void ValidateErrorScenariosForGiftCard() throws Exception
+//	{
+//		new GiftCardPage(driver).verifyerrormessagesongiftcard();
+//	}
+//
+//	@AfterMethod(alwaysRun = true)
+//	void teardown () {
+//
+//		driver.close();
+//		driver.quit();
+//	}
+//
+//}
